334daad6ebce805aac42fded9281013382e13b6d
liulingjun.godzilla <liulingjun.godzilla@bytedance.com>
2024-10-28 16:42:57 +0800

    free kv cache support

--- alpha_seed/workers/xperf_rollout/utils/weight_loader.py
+++ alpha_seed/workers/xperf_rollout/utils/weight_loader.py
@@ -56,6 +56,8 @@ def offload_to_cpu(tp_model):
         param_list.append(tp_model.wpe.weight)
     for param in param_list:
         param.data = torch.empty_like(param.data, device='cpu')
+    if hasattr(tp_model, '_free_kv_cache'):
+        tp_model._free_kv_cache()
 
     torch.cuda.empty_cache()
 
--- tests/infer/test_xperf_cuda_graph.py
+++ tests/infer/test_xperf_cuda_graph.py
@@ -4,11 +4,28 @@ import torch
 import logging
 import torch.nn.functional as F
 
+
+
+def offload_to_cpu(tp_model):
+    param_list = [tp_model.layernorm_weight, tp_model.wte_weight, tp_model.lm_head_weight] + \
+                    [p for layer in tp_model.layers_weight for p in layer if isinstance(p, torch.Tensor)]
+    if hasattr(tp_model, 'wpe'):
+        param_list.append(tp_model.wpe.weight)
+    for param in param_list:
+        param.data = torch.empty_like(param.data, device='cpu')
+
+    if hasattr(tp_model, '_free_kv_cache'):
+        tp_model._free_kv_cache()
+
+    torch.cuda.empty_cache()
+
 # torchrun --nproc_per_node 1 --master_port 9898 xperf_cuda_graph.py
 
 
 if __name__ == "__main__":
-    xperf_gpt.load_xperf_gpt()
+    # xperf_gpt.load_xperf_gpt()
+    path='./modules/libModules.so'
+    torch.classes.load_library(path)
 
     max_length = 256
     max_new_tokens = 128
@@ -21,7 +38,7 @@ if __name__ == "__main__":
         slot_block_size=256,
         use_vllm=False,
         context_split_len=1024,
-        enable_cuda_graph=True, # enable cuda graph
+        enable_cuda_graph=False, # enable cuda graph
     )
     generate_kwargs = dict(
         max_new_tokens=max_new_tokens,
@@ -32,7 +49,7 @@ if __name__ == "__main__":
         rank0_split_backend="nccl"
     )
     inference_sess.init_inference_engine(
-        "./config.json", generate_kwargs
+        "/opt/tiger/alpha-seed/tests/infer/config.json", generate_kwargs, enable_metrics=True
     )
     query_pool = ["猪能吃钛合金吗？", "你好"]
     timeout_seconds = 2
@@ -48,3 +65,8 @@ if __name__ == "__main__":
             print(f"idx: {v.idx} input: {v.input_prompt}\nunfinished timeout output: {v.new_token_ids}")
     torch.cuda.synchronize()
     print(f"Time spent: {time.time()-start}")
+
+    print("torch.cuda.memory_allocated: {} MB".format(torch.cuda.memory_allocated()/1024/1024))
+
+    offload_to_cpu(inference_sess.engine.module)
+    print("offloaded, torch.cuda.memory_allocated: {} MB".format(torch.cuda.memory_allocated()/1024/1024))
