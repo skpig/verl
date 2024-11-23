9fae3a5846644ea992739a96592db741d02675b3
zhangru.1994 <zhangru.1994@bytedance.com>
2024-11-23 20:23:57 +0800

    timeout*100

--- alpha_seed/utils/redis/triton_redis.py
+++ alpha_seed/utils/redis/triton_redis.py
@@ -15,4 +15,4 @@ class BytedRedisRemoteCacheBackend(RedisRemoteCacheBackend):
     def __init__(self, key):
         super().__init__(key)
         self._redis = bytedredis.Client.from_url(
-            f"redis://?db=0&redis_psm={BYTED_SEED_KV_PSM}&socket_connect_timeout=0.25&socket_timeout=0.3")
+            f"redis://?db=0&redis_psm={BYTED_SEED_KV_PSM}&socket_connect_timeout=25&socket_timeout=30")
