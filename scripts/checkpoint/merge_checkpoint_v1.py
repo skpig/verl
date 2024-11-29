7f12398d73be4c68a0735d88d8ebddb55ede993b
张驰 <zhangchi.usc1992@bytedance.com>
2024-11-29 10:00:43 +0000

    [ckpt] fix: fix merge for large models
    * update
    
    
    * update
    
    
    * update
    
    
    * update
    
    
    * update
    
    
    * update
    
    
    * update
    
    
    * update
    
    
    * update
    
    
    See merge request: !172

--- scripts/checkpoint/merge_checkpoint_v1.py
+++ scripts/checkpoint/merge_checkpoint_v1.py
@@ -1,79 +1,114 @@
-from verl.utils.fs import copy_local_path_from_hdfs
 import seed_models
 import os
-
 import torch
-import torch.distributed
-
+import torch.distributed._tensor
 import argparse
-
+import seed_models
 from transformers import AutoConfig, AutoModelForCausalLM
-
+from concurrent.futures import ThreadPoolExecutor
 import hdfs_io
-
 from tqdm.auto import trange
-
 from seed_models.commands.convert_to_megatron import convert_seed_models_to_megatron
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--hdfs_path', required=True)
-    parser.add_argument('--fsdp_size', required=False, type=int, default=-1)
+    parser.add_argument('--load-dir', required=True)
+    parser.add_argument('--save-path', required=False)
+    parser.add_argument('--cruise-config', required=False)
+    parser.add_argument('--dtype', required=False)
     args = parser.parse_args()
 
     print('Downloading model shards')
+    local_dir = '/opt/tiger/.cache/src_model'
+    os.makedirs(local_dir, exist_ok=True)
 
-    local_path = copy_local_path_from_hdfs(args.hdfs_path)
+    if not args.load_dir.endswith('actor'):
+        args.load_dir = os.path.join(args.load_dir, 'actor')
 
-    # find how many shards
-    files = [filename for filename in os.listdir(local_path) if filename.startswith('model_optim_rank')]
-    # to support HSDP with v1 ckpt
-    total_shards = len(files) if args.fsdp_size == -1 else args.fsdp_size
+    if not args.save_path:
+        args.save_path = os.path.join(args.load_dir, 'megatron_merge_states.pt')
 
-    print('Processing model shards')
+    # hdfs_io.copy(args.load_dir, local_dir)
+    hdfs_io.copy(os.path.join(args.load_dir, 'huggingface'), os.path.join(local_dir, 'huggingface'))
+
+    # copy rank zero to find the shape of (dp, fsdp)
+    rank = 0
+    hdfs_io.copy(os.path.join(args.load_dir, f'model_optim_rank_{rank}.pt'),
+                 os.path.join(local_dir, f'model_optim_rank_{rank}.pt'),
+                 chunk_thread_num=16)
+    state_dict = torch.load(os.path.join(local_dir, f'model_optim_rank_{rank}.pt'), map_location='cpu')['model']
+    pivot_key = sorted(list(state_dict.keys()))[0]
+    weight = state_dict[pivot_key]
+    assert isinstance(weight, torch.distributed._tensor.DTensor)
+    # get sharding info
+    device_mesh = weight.device_mesh
+    mesh = device_mesh.mesh
+    mesh_dim_names = device_mesh.mesh_dim_names
+
+    print(f'Got device mesh {mesh}, mesh_dim_names {mesh_dim_names}')
+
+    assert mesh_dim_names in (('fsdp',), ('dp', 'fsdp'))
+
+    total_shards = mesh.shape[-1]
+
+    print(f'Processing model shards with {total_shards} in total')
 
     model_state_dict_lst = []
-    for rank in trange(total_shards, desc='Loading model shards'):
-        model_path = os.path.join(local_path, f'model_optim_rank_{rank}.pt')
+    model_state_dict_lst.append(state_dict)
+    model_state_dict_lst.extend([""] * (total_shards - 1))
+
+    def process_one_shard(rank):
+        hdfs_io.copy(os.path.join(args.load_dir, f'model_optim_rank_{rank}.pt'),
+                     os.path.join(local_dir, f'model_optim_rank_{rank}.pt'),
+                     chunk_thread_num=16)
+        model_path = os.path.join(local_dir, f'model_optim_rank_{rank}.pt')
         state_dict = torch.load(model_path, map_location='cpu')
-        model_state_dict_lst.append(state_dict['model'])
+        model_state_dict_lst[rank] = state_dict['model']
+        os.remove(model_path)
+
+    with ThreadPoolExecutor(max_workers=32) as executor:
+        for rank in trange(1, total_shards, desc='Loading model shards'):
+            executor.submit(process_one_shard, rank)
 
     # reorder model_state_dict based on keys
-
     state_dict = {}
-
     keys = set(model_state_dict_lst[0].keys())
-
     for key in keys:
         state_dict[key] = []
         for model_state_dict in model_state_dict_lst:
             tensor = model_state_dict.pop(key)
             if isinstance(tensor, torch.distributed._tensor.DTensor):
-                state_dict[key].append(tensor._local_tensor)
+                state_dict[key].append(tensor._local_tensor.bfloat16())
             else:
-                state_dict[key] = tensor
+                state_dict[key] = tensor.bfloat16()
+
+    del model_state_dict_lst
 
     for key in sorted(state_dict):
         # FSDP is shard-0 dtensor
         if isinstance(state_dict[key], list):
             print(f'Concat key {key}')
             state_dict[key] = torch.cat(state_dict[key], dim=0)
-
     print('Writing to local disk')
-
-    hf_path = os.path.join(local_path, 'huggingface')
+    hf_path = os.path.join(local_dir, 'huggingface')
     config = AutoConfig.from_pretrained(hf_path)
-    model = AutoModelForCausalLM.from_config(config, torch_dtype=torch.float32)
+
+    with torch.device('meta'):
+        model = AutoModelForCausalLM.from_config(config, torch_dtype=torch.bfloat16)
+    model.to_empty(device='cpu')
 
     print(f'Saving model to {hf_path}')
     model.save_pretrained(hf_path, state_dict=state_dict)
 
-    print(f'Upload merged huggingface model from {hf_path} to {args.hdfs_path}')
+    del state_dict
+    del model
 
+    # print(f'Upload merged huggingface model from {hf_path} to {args.hdfs_path}')
     # upload back to hdfs
-    hdfs_io.copy(hf_path, args.hdfs_path)
-
-    print(f'Upload merged megatron model from {hf_path} to {args.hdfs_path}')
-
+    # hdfs_io.copy(hf_path, args.hdfs_path)
+    print(f'Upload merged megatron model from {hf_path} to {args.save_path}')
     # convert to megatron for autoeval
-    convert_seed_models_to_megatron(hf_path=hf_path, local_path=hf_path, output_path=args.hdfs_path)
+    convert_seed_models_to_megatron(hf_path=hf_path,
+                                    local_path=local_dir,
+                                    output_path=os.path.dirname(args.save_path),
+                                    validate=False)
