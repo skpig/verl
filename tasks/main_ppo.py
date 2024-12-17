786fac8e0b6fb12eb07788c045995ebce31c55cd
禹棋赢 <qiying.01@bytedance.com>
2024-12-17 13:33:30 +0000

    [rm] fix: is_trunc fix a tricky bool of torch.bool and bool
    * fix a tricky bool of torch.bool and bool
    
    
    * fix is_trunc reward
    
    
    See merge request: !265

--- tasks/main_ppo.py
+++ tasks/main_ppo.py
@@ -145,7 +145,7 @@ class RewardManager():
                 score_fn_inputs["code_sandbox_psm"] = self.config.trainer.code_sandbox_psm
             score = compute_score_fn(**score_fn_inputs)
             is_para_dup = para_dup.find_single_turn_duplicate(solution_str)[0]
-            is_trunc = (response_length == valid_response_length) and score == -1
+            is_trunc = (response_length == valid_response_length).item() and score == -1
             return prompt_str, solution_str, ground_truth, reward_style, valid_response_length, score, is_para_dup, is_trunc, idx, solution_str_post_proc
 
         for i in range(len(data)):
@@ -191,7 +191,7 @@ class RewardManager():
                     [global_step, prompt_str, solution_str, ground_truth, score, solution_str_post_proc])
             save_to_hdfs.append([
                 idx, global_step, prompt_str, solution_str, ground_truth, score, solution_str_post_proc, is_para_dup,
-                is_trunc.item(),
+                is_trunc,
                 valid_response_length.item()
             ])
 
