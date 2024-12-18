44eabd9aca34136d318e3731fb55a42af901b48e
禹棋赢 <qiying.01@bytedance.com>
2024-12-18 12:12:47 +0000

    [misc] feat: add raw score metric
    * fix eval
    
    
    * add raw score metric
    
    
    See merge request: !275

--- tasks/main_ppo.py
+++ tasks/main_ppo.py
@@ -151,6 +151,7 @@ class RewardManager():
         response_ids = data.batch['input_ids'][:, self.config.data.max_prompt_length:]
 
         reward_tensor = torch.zeros_like(response_ids, dtype=torch.float32)
+        raw_scores = torch.zeros_like(response_ids, dtype=torch.float32)
         already_print_data_sources = {}
         save_to_hdfs = []
         rm_res_future_list = []
@@ -219,6 +220,7 @@ class RewardManager():
             # eval的时候不做这个norm
             if need_norm:
                 score = (score - self.mean) / self.std
+            raw_score = score
             if is_para_dup:
                 dup_cnt += 1
                 dup_lens.append(valid_response_length)
@@ -229,6 +231,7 @@ class RewardManager():
             if self.need_punish_trunc and is_trunc and not is_validation:
                 score = self.trunc_punish_score
             reward_tensor[idx, valid_response_length - 1] = score
+            raw_scores[idx, valid_response_length - 1] = raw_score
 
             if reward_style not in already_print_data_sources:
                 already_print_data_sources[reward_style] = 0
@@ -286,7 +289,10 @@ class RewardManager():
             p.start()
             print(f"reward_fn end hput: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
-        return reward_tensor
+        if not is_validation:
+            return reward_tensor, raw_scores
+        else:
+            return reward_tensor
 
 
 import ray
