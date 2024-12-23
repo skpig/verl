d5e9c43e300ad08e072f8d1274bbaef0c70a5763
qiying.01 <qiying.01@bytedance.com>
2024-12-23 10:58:54 +0800

    fix gpqa verifier

--- alpha_seed/utils/reward_score/gpqa_verifier.py
+++ alpha_seed/utils/reward_score/gpqa_verifier.py
@@ -6,7 +6,7 @@ from alpha_seed.utils.reward_score.math_v2 import last_boxed_only_string_v2, rem
 
 
 def compute_score(solution_str, ground_truth, **argv) -> float:
-    pred = last_boxed_only_string_v2(solution_str[:-100])
+    pred = last_boxed_only_string_v2(solution_str[-100:])
     if pred is None:
         return 0
     pred = remove_boxed(pred)
