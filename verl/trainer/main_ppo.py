a09293a83c107ffbf3bbe8b42994d5fb7d2db66d
basilH <basilH@local.com>
2025-07-11 10:58:56 +0800

    [fix] remove ray init args

--- verl/trainer/main_ppo.py
+++ verl/trainer/main_ppo.py
@@ -69,10 +69,10 @@ def run_ppo(config) -> None:
     if not ray.is_initialized():
         # this is for local ray cluster
         ray.init(
-            runtime_env={
-                "env_vars": {"TOKENIZERS_PARALLELISM": "true", "NCCL_DEBUG": "WARN", "VLLM_LOGGING_LEVEL": "WARN", 'CUDA_VISIBLE_DEVICES': os.environ.get("CUDA_VISIBLE_DEVICES", "0")}
-            },
-            num_cpus=config.ray_init.num_cpus,
+            # runtime_env={
+            #     "env_vars": {"TOKENIZERS_PARALLELISM": "true", "NCCL_DEBUG": "WARN", "VLLM_LOGGING_LEVEL": "WARN", 'CUDA_VISIBLE_DEVICES': os.environ.get("CUDA_VISIBLE_DEVICES", "0")}
+            # },
+            # num_cpus=config.ray_init.num_cpus,
         )
 
     runner = TaskRunner.remote()
