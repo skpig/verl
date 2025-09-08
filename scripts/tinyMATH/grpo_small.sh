4872f776df642ab5616d55f54991d50bc769ced2
huangbaizhou <huangbaizhou@bytedance.com>
2025-09-08 15:55:37 +0800

    update scripts

--- scripts/tinyMATH/grpo_small.sh
+++ scripts/tinyMATH/grpo_small.sh
@@ -1,4 +1,4 @@
-RUN_ID=6
+RUN_ID=22
 WANDB_VERSION=bwandb
 # one node
 FORWARD_RATIO=10
@@ -14,7 +14,7 @@ VAL_TOPK=20
 
 # Model settings
 USE_OVERLONG=True
-SAMPLER=None # null, tree, mopps
+SAMPLER=tree # null, tree, mopps
 DATA_WORKERS=0
 CRITIC_WARMUP=0
 PROMPT_ID=4
@@ -28,15 +28,17 @@ MAX_RESPONSE_LEN=$((1024 * 5 + OVERLONG_BUFFER_LEN))
 CLIP_HIGHER=0.28
 
 # Tree Sampler settings
-TREE_SAMPLER=epsilon # mcts pg
+TREE_SAMPLER=pg # mcts pg
 EPSILON=0.2
 
 # Tree Selector
-TREE_SELECTOR=value # value entropy mix, mix2
+TREE_SELECTOR=entropy # value entropy mix, mix2
 ROLLOUT_RATIO=0.7
 INCORRECT_PROB=0.3
-ROOT_ONLY=False
-
+ROOT_ONLY=True
+DIV_THRESHOLD=3 # 0 by default
+NUM_GIBBS=100
+GIBBS_DISCOUNT=0.99
 
 # Performance tuning
 N_NODES=${ARNOLD_WORKER_NUM:-1}
@@ -104,6 +106,9 @@ CMD="python3 -m verl.trainer.main_ppo \
     data.truncation='error' \
     data.sampler.tree_sampler.name=${TREE_SAMPLER} \
     data.sampler.tree_sampler.epsilon=${EPSILON} \
+    data.sampler.tree_sampler.diverse_threshold=${DIV_THRESHOLD} \
+    data.sampler.tree_sampler.gibbs_sweeps=${NUM_GIBBS} \
+    data.sampler.tree_sampler.gamma=${GIBBS_DISCOUNT} \
     data.tree_data.partial_rollout_ratio=${ROLLOUT_RATIO} \
     data.tree_data.keep_incorrect_prob=${INCORRECT_PROB} \
     data.tree_data.root_only=${ROOT_ONLY} \
@@ -126,7 +131,7 @@ CMD="python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=$FORWARD_MAX_TOKEN_LEN \
     actor_rollout_ref.rollout.tensor_model_parallel_size=$ROLLOUT_TP_SIZE \
     actor_rollout_ref.rollout.name=sglang \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.65 \
     actor_rollout_ref.rollout.n=$ROLLOUT_N \
     actor_rollout_ref.rollout.val_kwargs.temperature=${VAL_TEMP} \
     actor_rollout_ref.rollout.val_kwargs.top_k=${VAL_TOPK} \
--- scripts/tinyMATH/ppo_small.sh
+++ scripts/tinyMATH/ppo_small.sh
@@ -1,4 +1,4 @@
-RUN_ID=4
+RUN_ID=21
 WANDB_VERSION=bwandb
 # one node
 FORWARD_RATIO=10
@@ -14,7 +14,7 @@ VAL_TOPK=20
 
 # Model settings
 USE_OVERLONG=True
-SAMPLER=None # null, tree, mopps
+SAMPLER=tree # null, tree, mopps
 DATA_WORKERS=0
 CRITIC_WARMUP=0
 PROMPT_ID=4
@@ -28,14 +28,17 @@ MAX_RESPONSE_LEN=$((1024 * 5 + OVERLONG_BUFFER_LEN))
 CLIP_HIGHER=0.28
 
 # Tree Sampler settings
-TREE_SAMPLER=epsilon # mcts pg
+TREE_SAMPLER=pg # mcts pg
 EPSILON=0.2
 
 # Tree Selector
-TREE_SELECTOR=value # entropy mix1
+TREE_SELECTOR=mix2 # value entropy mix, mix2
 ROLLOUT_RATIO=0.7
-
-
+INCORRECT_PROB=0.3
+ROOT_ONLY=True
+DIV_THRESHOLD=3 # 0 by default
+NUM_GIBBS=100
+GIBBS_DISCOUNT=0.99
 
 # Performance tuning
 N_NODES=${ARNOLD_WORKER_NUM:-1}
@@ -108,7 +111,12 @@ CMD="python3 -m verl.trainer.main_ppo \
     data.truncation='error' \
     data.sampler.tree_sampler.name=${TREE_SAMPLER} \
     data.sampler.tree_sampler.epsilon=${EPSILON} \
+    data.sampler.tree_sampler.diverse_threshold=${DIV_THRESHOLD} \
+    data.sampler.tree_sampler.gibbs_sweeps=${NUM_GIBBS} \
+    data.sampler.tree_sampler.gamma=${GIBBS_DISCOUNT} \
     data.tree_data.partial_rollout_ratio=${ROLLOUT_RATIO} \
+    data.tree_data.keep_incorrect_prob=${INCORRECT_PROB} \
+    data.tree_data.root_only=${ROOT_ONLY} \
     data.tree_data.name=${TREE_SELECTOR} \
     actor_rollout_ref.model.path=$BASE_MODEL \
     actor_rollout_ref.model.use_remove_padding=True \
@@ -127,7 +135,7 @@ CMD="python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=$FORWARD_MAX_TOKEN_LEN \
     actor_rollout_ref.rollout.tensor_model_parallel_size=$ROLLOUT_TP_SIZE \
     actor_rollout_ref.rollout.name=sglang \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.65 \
     actor_rollout_ref.rollout.n=$ROLLOUT_N \
     actor_rollout_ref.rollout.val_kwargs.temperature=${VAL_TEMP} \
     actor_rollout_ref.rollout.val_kwargs.top_k=${VAL_TOPK} \
